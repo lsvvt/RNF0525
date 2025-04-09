d051a9a9f0711937007fd45e3effb17629ac3954
lsvvt <losev.tim.v@gmail.com>
2025-04-09 13:17:47 +0300

    UKS

--- run.py
+++ run.py
@@ -68,14 +68,30 @@ def build_pyscf_mol(geometry_data, basis_set):
     return mol
 
 def eval_gga_xc_pidl(xc_code, rho, spin=0, relativity=0, deriv=2, omega=None, verbose=None):
-    inp = {}
-    inp["rho"] = rho[0]
-    inp["sigma"] = rho[1]**2 + rho[2]**2 + rho[3]**2
-    inp["tau"] = rho[5]
+    rhoa, rhob = rho
+
+    npl = len(rhoa[0])
+
+    rho = np.zeros(npl * 2)
+    sigma = np.zeros(npl * 3)
+    lapl = np.zeros(npl * 2)
+    tau = np.zeros(npl * 2)
+
+    for i in range(npl):
+        rho[i * 2 + 0] = rhoa[0][i]
+        rho[i * 2 + 1] = rhob[0][i]
+        sigma[i * 3 + 0] = rhoa[1][i] * rhoa[1][i] + rhoa[2][i] * rhoa[2][i] + rhoa[3][i] * rhoa[3][i]
+        sigma[i * 3 + 1] = rhoa[1][i] * rhob[1][i] + rhoa[2][i] * rhob[2][i] + rhoa[3][i] * rhob[3][i]
+        sigma[i * 3 + 2] = rhob[1][i] * rhob[1][i] + rhob[2][i] * rhob[2][i] + rhob[3][i] * rhob[3][i]
+        lapl[i * 2 + 0] = rhoa[4][i]
+        lapl[i * 2 + 1] = rhob[4][i]
+        tau[i * 2 + 0] = rhoa[5][i]
+        tau[i * 2 + 1] = rhob[5][i]
+
 
     params = np.load("par_pbe0pbe0_old_d3bj.npy")[-1][1:]
-    funcc = pylibxc.LibXCFunctional("MGGA_C_M06_2X", "unpolarized")
-    funcx = pylibxc.LibXCFunctional("HYB_MGGA_X_M06_2X", "unpolarized")
+    funcc = pylibxc.LibXCFunctional("MGGA_C_M06_2X", "polarized")
+    funcx = pylibxc.LibXCFunctional("HYB_MGGA_X_M06_2X", "polarized")
 
     ext_c = np.zeros(27)
     for i in range(22):
@@ -93,31 +109,51 @@ def eval_gga_xc_pidl(xc_code, rho, spin=0, relativity=0, deriv=2, omega=None, ve
     funcc.set_ext_params(np.array(ext_c))
     funcx.set_ext_params(np.array(ext_x))
 
+    inp = {"rho": rho, "sigma": sigma, "tau": tau}
     retx = funcx.compute(inp)
     retc = funcc.compute(inp)
 
-    vrho = retx["vrho"].flatten() + retc["vrho"].flatten()
-    vgamma = retx["vsigma"].flatten() + retc["vsigma"].flatten()
-    vtau = retx["vtau"].flatten() + retc["vtau"].flatten()
+    exc = retx['zk'] + retc['zk']
+
+    vxc, fxc, kxc = None, None, None
+
+    vrho = retx["vrho"] + retc["vrho"]       # Shape (Ngrid, 2)
+    vsigma = retx["vsigma"] + retc["vsigma"] # Shape (Ngrid, 3)
+    vtau = retx["vtau"] + retc["vtau"]       # Shape (Ngrid, 2)
+
     vlapl = None
-    vxc = (vrho, vgamma, vlapl  , vtau)
-    exc = retx["zk"].flatten() + retc["zk"].flatten()
+    vxc = (vrho, vsigma, vlapl, vtau)
 
     fxc = None
     kxc = None
-
     return exc, vxc, fxc, kxc
 
 
 def eval_gga_xc_pi(xc_code, rho, spin=0, relativity=0, deriv=2, omega=None, verbose=None):
-    inp = {}
-    inp["rho"] = rho[0]
-    inp["sigma"] = rho[1]**2 + rho[2]**2 + rho[3]**2
-    inp["tau"] = rho[5]
+    rhoa, rhob = rho
+
+    npl = len(rhoa[0])
+
+    rho = np.zeros(npl * 2)
+    sigma = np.zeros(npl * 3)
+    lapl = np.zeros(npl * 2)
+    tau = np.zeros(npl * 2)
+
+    for i in range(npl):
+        rho[i * 2 + 0] = rhoa[0][i]
+        rho[i * 2 + 1] = rhob[0][i]
+        sigma[i * 3 + 0] = rhoa[1][i] * rhoa[1][i] + rhoa[2][i] * rhoa[2][i] + rhoa[3][i] * rhoa[3][i]
+        sigma[i * 3 + 1] = rhoa[1][i] * rhob[1][i] + rhoa[2][i] * rhob[2][i] + rhoa[3][i] * rhob[3][i]
+        sigma[i * 3 + 2] = rhob[1][i] * rhob[1][i] + rhob[2][i] * rhob[2][i] + rhob[3][i] * rhob[3][i]
+        lapl[i * 2 + 0] = rhoa[4][i]
+        lapl[i * 2 + 1] = rhob[4][i]
+        tau[i * 2 + 0] = rhoa[5][i]
+        tau[i * 2 + 1] = rhob[5][i]
+
 
     params = np.load("par_pbe0pbe0_old.npy")[-5][1:]
-    funcc = pylibxc.LibXCFunctional("MGGA_C_M06_2X", "unpolarized")
-    funcx = pylibxc.LibXCFunctional("HYB_MGGA_X_M06_2X", "unpolarized")
+    funcc = pylibxc.LibXCFunctional("MGGA_C_M06_2X", "polarized")
+    funcx = pylibxc.LibXCFunctional("HYB_MGGA_X_M06_2X", "polarized")
 
     ext_c = np.zeros(27)
     for i in range(22):
@@ -135,19 +171,23 @@ def eval_gga_xc_pi(xc_code, rho, spin=0, relativity=0, deriv=2, omega=None, verb
     funcc.set_ext_params(np.array(ext_c))
     funcx.set_ext_params(np.array(ext_x))
 
+    inp = {"rho": rho, "sigma": sigma, "tau": tau}
     retx = funcx.compute(inp)
     retc = funcc.compute(inp)
 
-    vrho = retx["vrho"].flatten() + retc["vrho"].flatten()
-    vgamma = retx["vsigma"].flatten() + retc["vsigma"].flatten()
-    vtau = retx["vtau"].flatten() + retc["vtau"].flatten()
+    exc = retx['zk'] + retc['zk']
+
+    vxc, fxc, kxc = None, None, None
+
+    vrho = retx["vrho"] + retc["vrho"]       # Shape (Ngrid, 2)
+    vsigma = retx["vsigma"] + retc["vsigma"] # Shape (Ngrid, 3)
+    vtau = retx["vtau"] + retc["vtau"]       # Shape (Ngrid, 2)
+
     vlapl = None
-    vxc = (vrho, vgamma, vlapl  , vtau)
-    exc = retx["zk"].flatten() + retc["zk"].flatten()
+    vxc = (vrho, vsigma, vlapl, vtau)
 
     fxc = None
     kxc = None
-
     return exc, vxc, fxc, kxc
 
 # @memory.cache
@@ -250,7 +290,10 @@ def main(basis_set, xc, fyaml):
         # Сравниваем с reference_energy_kcal_mol
         ref_energy = rxn["details"].get("reference_energy_kcal_mol", None)
 
-        error = E_calc_diff_kcalmol - ref_energy
+        if fyaml == "h4_database.yaml":
+            error = E_calc_diff_kcalmol
+        else:
+            error = E_calc_diff_kcalmol - ref_energy
         abs_errors.append(abs(error))
 
         # print(f"Реакция: {rxn['id']} / Расчёт: {E_calc_diff_kcalmol:.2f} кдж/моль / "
@@ -290,6 +333,8 @@ if __name__ == "__main__":
             basis_set = "cc-pVQZ"
         elif fyaml == "hocl_dissociation_database.yaml":
             basis_set = "aug-cc-pVTZ"
+        elif fyaml == "h4_database.yaml":
+            basis_set = "6-31G"
 
         table["basis_set"] = basis_set
 
